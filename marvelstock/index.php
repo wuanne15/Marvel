4d16e2a2634ca1b6afe58ff40be0d587062ddac1
cmdjlee <joshleelee@gmail.com>
2017-10-22 12:15:16 -0700

    bugs

--- marvelstock/index.php
+++ marvelstock/index.php
@@ -3,9 +3,9 @@
   printTop();
 ?>
       <script type="text/javascript" src="index.js"></script>
-      <!--<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
       <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-      <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>-->
+      <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
       <h1>Welcome to Stock Marvel</h1>
       <p>This is some text.</p>
       <div class="row">
@@ -29,22 +29,6 @@
               </tr>
             </thead>
             <tbody>
-            <!--
-            #$db = connect();
-            #$rows = $db->query("
-								#SELECT
-								#FROM
-								#WHERE
-								#LIMIT 6");
-            #for ($i = 1; $i <= 6; $i++) {
-            	#$row= $rows->fetch(); ?>
-            	<tr>
-                	<td><?=#$i?></td>
-                	<td><?=#$row[""]?></td>
-                	<td class="text-right">3000</td>
-                	<td class="text-right">+0</td>
-              </tr>
-            }-->
               <tr>
                 <td>1</td>
                 <td>Iron Man</td>
