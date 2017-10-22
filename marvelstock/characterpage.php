3b1d431414a5999ccd00852c740bef2e4f6ddf35
cmdjlee <joshleelee@gmail.com>
2017-10-22 13:27:50 -0700

    purchase

--- marvelstock/characterpage.php
+++ marvelstock/characterpage.php
@@ -20,6 +20,35 @@
                     CardValue
                   </div>
                   <div class="col-lg-6">
+                    <?php if (loggedIn()) {
+                      if (hasCard){
+                        ?>
+                        <p class="text-right">Owned</p>
+                        <?php
+                      } else {
+                        ?>
+                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#purchase">Purchase</button>
+                        <?php
+                      }
+                    }
+                    ?>
+
+                    <div class="modal fade" id="Purchase" role="dialog">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">Purchase</button>
+                            <h4 class="modal-title">Hero Name</h4>
+                          </div>
+                          <div class="modal-body">
+                            <p>Purchsae </p>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                     <p class="text-right">Owned/Unowned</p>
                   </div>
                 </div>
