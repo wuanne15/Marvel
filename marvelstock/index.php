a51150ddcb79f3eec160e9cdaaa70bc9546b798f
cmdjlee <joshleelee@gmail.com>
2017-10-22 12:16:19 -0700

    buggg

--- marvelstock/index.php
+++ marvelstock/index.php
@@ -2,7 +2,7 @@
   include("common.php");
   printTop();
 ?>
-      <script type="text/javascript" src="index.js"></script>
+      <!--<script type="text/javascript" src="index.js"></script>-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
       <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
       <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
